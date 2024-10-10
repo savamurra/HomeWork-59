71ac212cd374de10ddf48f94edaaeae633727b1c
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-10 16:24:23 +0600

    Вывзал компоненты

---
+++ src/containers/Anecdote/Anecdote.tsx
@@ -0,0 +1,11 @@
+import AnecdoteItem from '../../components/AnecdoteItem/AnecdoteItem.tsx';
+
+const Anecdote = () => {
+  return (
+    <div>
+      <AnecdoteItem/>
+    </div>
+  );
+};
+
+export default Anecdote;

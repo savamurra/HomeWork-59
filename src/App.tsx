f11d0b847d4ff2b9f683bb156db04432904566f3
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-10 16:44:06 +0600

    Добавил мини роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import Movie from './containers/Movie/Movie.tsx';
 import Anecdote from './containers/Anecdote/Anecdote.tsx';
+import { useState } from 'react';
 
 
-const App = () => (
+const App = () => {
+
+  const [changeComponents, setChangeComponents] = useState<boolean>(false);
+
+
+  return (
   <>
-    <Movie/>
-    <Anecdote/>
+    <div className="container d-flex flex-column align-items-center">
+      <button className="btn btn-primary mt-4" style={{width: '100px'}} onClick={() => setChangeComponents(!changeComponents)}>{changeComponents ? 'Anecdote' : 'Movies'}</button>
+      {changeComponents ? <Anecdote/> : <Movie/>}
+    </div>
   </>
-);
+  )
+};
 
 export default App
